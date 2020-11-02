1335a70f239f25bd7641b3f7b26856b323b2026b
Chris Feasby <cfeasby204@gmail.com>
2020-11-02 13:40:51 -0600

    Set up category and product controller index and show pages as well as defined some routes for them.

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,7 +1,10 @@
 class ProductController < ApplicationController
   def index
+    @products = Product.includes(:category).all
   end
 
   def show
+    @product = Product.find(params[:id])
+    @show_link = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'category/index'
+  get 'category/show'
   get 'product/index'
-  get 'product/show'
+  get "/product/show/(:id)", to: "product#show", as: "product"
+  resources :category, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
