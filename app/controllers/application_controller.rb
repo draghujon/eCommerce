081c37a0da8cfa674afda21da35d2eadb8bb28c6
Chris Feasby <cfeasby204@gmail.com>
2020-11-16 18:23:20 -0600

    Got provinces to appear in a drop down list.. however it doesn't work.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  protect_from_forgery with: :exception
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up){|u| u.permit(:address, :province)}
-      devise_parameter_sanitizer.permit(:account_update){|u| u.permit(:address, :province)}
+      permit_attrs(%i[address province id])
+    end
+
+    def permit_attrs(attrs)
+      %i[sign_up account_update].each do |action|
+        devise_parameter_sanitizer.permit(action, keys: attrs)
+      end
     end
 end
